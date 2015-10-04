70a5647c11d6ef9d94e625f710b4f2342d48df44
Huy Tran <kingbazoka@gmail.com>
2015-10-04 16:09:21 -0700

    Implement instructions

--- app.js
+++ app.js
@@ -15,6 +15,11 @@ window.requestAnimFrame = (function(){
 var Chip8 = function() {
     this.pc = 0;
     this.memory = new Uint8Array(new ArrayBuffer(0x1000)); // 4Kb RAM
+    this.v = new Array(16);
+    this.i = null;
+    this.stack = new Array(16);
+    this.sp = null;
+
     this.reset();
 };
 
@@ -46,7 +51,13 @@ Chip8.prototype.reset = function() {
         this.memory[i] = hexChars[i];
     }
 
+    for (i = 0; i < this.v.length; i++) {
+      this.v[i] = 0;
+    }
+
     this.pc = 0x200;
+    this.sp = 0;
+    this.i = 0;
 };
 
 Chip8.prototype.open = function(url) {
@@ -83,11 +94,11 @@ Chip8.prototype.cycle = function() {
 
     // Read the variables
     // n - 0x000F
-    var n = (opcode & 0x000F).toString(16);
+    var n = opcode & 0x000F;
     // nnn - 0x0FFF
-    var nnn = (opcode & 0x0FFF).toString(16);
+    var nnn = opcode & 0x0FFF;
     // kk - 0x00FF
-    var kk = (opcode & 0x00FF).toString(16);
+    var kk = opcode & 0x00FF;
     // x - 0x0F00
     var x = opcode & 0x0F00 >> 8;
     // y - 0x00F0
@@ -110,36 +121,56 @@ Chip8.prototype.cycle = function() {
                 // 00EE - RET
                 case 0x00EE:
                     console.doc("RET");
+                    this.pc = this.stack[--this.sp];
                     break;
             }
             break;
         // 1nnn - JP addr
         case 0x1000:
             console.doc("JP " + nnn);
+            this.pc = nnn;
             break;
         // 2nnn - CALL addr
         case 0x2000:
             console.doc("CALL " + nnn);
+            this.stack[this.sp] = this.pc;
+            this.sp++;
+            this.pc = nnn;
             break;
         // 3xkk - SE Vx, byte
         case 0x3000:
             console.doc("SE V" + x + ", " + kk);
+            if (this.v[x] === kk) {
+              this.pc += 2;
+            }
             break;
         // 4xkk - SNE Vx, byte
         case 0x4000:
             console.doc("SNE V" + x + ", " + kk);
+            if (this.v[x] != kk) {
+              this.pc += 2;
+            }
             break;
         // 5xy0 - SE Vx, Vy
         case 0x5000:
             console.doc("SE V" + x + ", V" + y);
+            if (this.v[x] === this.v[y]) {
+                this.pc += 2;
+            }
             break;
         // 6xkk - LD Vx, byte
         case 0x6000:
             console.doc("LD V" + x + ", " + kk);
+            this.v[x] = kk;
             break;
         // 7xkk - ADDR Vx, byte
         case 0x7000:
             console.doc("ADDR V" + x + ", " + kk);
+            var val = kk + this.v[x]
+            if (val > 255) {
+                val -= 256;
+            }
+            this.v[x] = val;
             break;
         // 8xy*
         case 0x8000:
@@ -147,38 +178,64 @@ Chip8.prototype.cycle = function() {
                 // 8xy0 - LD Vx, Vy
                 case 0x0000:
                     console.doc("LD V" + x + ", V" + y);
+                    this.v[x] = this.v[y];
                     break;
                 // 8xy1 - OR Vx, Vy
                 case 0x0001:
                     console.doc("OR V" + x + ", V" + y);
+                    this.v[x] |= this.v[y];
                     break;
                 // 8xy2 - AND Vx, Vy
                 case 0x0002:
                     console.doc("AND V" + x + ", V" + y);
+                    this.v[x] &= this.v[y];
                     break;
                 // 8xy3 - XOR Vx, Vy
                 case 0x0003:
                     console.doc("XOR V" + x + ", V" + y);
+                    this.v[x] ^= this.v[y];
                     break;
-                // 8xy4 - AD Vx, Vy
+                // 8xy4 - ADD Vx, Vy
                 case 0x0004:
                     console.doc("AD V" + x + ", V" + y);
+                    this.v[x] += this.v[y];
+                    this.v[0xF] = +(this.v[x] > 255);
+                    if (this.v[x] > 255) {
+                      this.v[x] -= 256;
+                    }
                     break;
                 // 8xy5 - SUB Vx, Vy
                 case 0x0005:
                     console.doc("SUB V" + x + ", V" + y);
+                    this.v[0xF] = +(this.v[x] > this.v[y]);
+                    this.v[x] -= this.v[y];
+                    if (this.v[x] < 0) {
+                      this.v[x] += 256;
+                    }
                     break;
                 // 8xy6 - SHR Vx {, Vy}
                 case 0x0006:
                     console.doc("SHR V" + x + ", V" + y);
+                    this.v[0xF] = this.v[x] & 0x1;
+                    this.v[x] >>= 1;
                     break;
                 // 8xy7 - SUBN Vx, Vy
                 case 0x0007:
                     console.doc("SUBN V" + x + ", V" + y);
+                    this.v[0xF] = +(this.v[y] > this.v[x]);
+                    this.v[x] = this.v[y] - this.v[x];
+                    if (this.v[x] < 0) {
+                      this.v[x] += 256;
+                    }
                     break;
                 // 8xyE - SHL Vx {, Vy}
                 case 0x000E:
                     console.doc("SHL V" + x + ", V" + y);
+                    this.v[0xF] = +(this.v[x] & 0x80);
+                    this.v[x] <<= 1;
+                    if (this.v[x] > 255) {
+                      this.v[x] -= 256;
+                    }
                     break;
             }
             break;
@@ -186,18 +243,24 @@ Chip8.prototype.cycle = function() {
         case 0x9000:
             // 9xy0 - SNE Vx, Vy
             console.doc("SNE V" + x + ", V" + y);
+            if (this.v[x] != this.v[y]) {
+              this.pc += 2;
+            }
             break;
         // Annn - LD I, addr
         case 0xA000:
             console.doc("LD I, " + nnn);
+            this.i = nnn;
             break;
         // Bnnn - JP V0, addr
         case 0xB000:
             console.doc("JP V0, " + nnn);
+            this.pc = nnn + this.v[0];
             break;
         // Cxkk - RND Vx, byte
         case 0xC000:
             console.doc("RND V" + x + ", " + kk);
+            this.v[x] = Math.floor(Math.random() * 0xFF) & kk
             break;
         // Dxyn - DRW Vx, Vy, nibble
         case 0xD000:
@@ -209,6 +272,7 @@ Chip8.prototype.cycle = function() {
                 // Ex9E - SKP Vx
                 case 0x009E:
                     console.doc("SKP V" + x);
+
                     break;
                 // ExA1 - SKNP Vx
                 case 0x00A1:
@@ -238,22 +302,35 @@ Chip8.prototype.cycle = function() {
                 // Fx1E - ADD I, Vx
                 case 0x001E:
                     console.log("ADD I, V" + x);
+                    this.i += this.v[x];
                     break;
                 // Fx29 - LD F, Vx
                 case 0x0029:
                     console.log("LD F, V" + x);
+                    this.i = this.v[x] * 5;
                     break;
                 // Fx33 - LD B, Vx
                 case 0x0033:
                     console.log("LD B, V" + x);
+                    var number = this.v[x], i;
+                    for (i = 3; i > 0; i--) {
+                      this.memory[this.i + i - 1] = parseInt(number % 10);
+                      number /= 10;
+                    }
                     break;
                 // Fx55 - LD [I], Vx
                 case 0x0055:
                     console.log("LD [I], V" + x);
+                    for (var i = 0; i <= x; i++) {
+                      this.memory[this.i + i] = this.v[i];
+                    }
                     break;
                 // Fx65 - LD Vx, [I]
                 case 0x0065:
                     console.log("LD V" + x + ", [I]");
+                    for (var i = 0; i <= x; i++) {
+                      this.v[i] = this.memory[this.i + i];
+                    }
                     break;
             }
             break;
